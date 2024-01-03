0fd0b4fd50db81dd23b60edda216e044369d822b
Tapabrata Goswami <tapbratagoswami21@gmail.com>
2024-01-04 02:00:38 +0530

    initial

--- linkefi-api/api/link-generator.php
+++ linkefi-api/api/link-generator.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Methods,Access-Control-Allow-Headers, Authorization, Access-Control-Allow-Origin');
 
-require_once ('../dbconnect.php');
+// require_once ('../dbconnect.php');
 
 $data = json_decode(file_get_contents('php://input'),true);
 
--- linkefi-api/index.php
+++ linkefi-api/index.php
@@ -3,24 +3,23 @@
 // Slience is golden
 
 require_once ('dbconnect.php');
-
 // Fetch the requested URL
-$request = $_GET['url'] ?? '/';
+$request = $_SERVER['REQUEST_URI'];
 
-// Define your routes (simple mapping of URLs to actions)
+// Define your routes (URL mappings to actions)
 $routes = [
     '/' => 'controller/link-counter.php',
-    // '/contact' => 'contact.php'
+    '/link-generate' => 'api/link-generator.php',
     // Add more routes as needed
 ];
 
 // Check if the requested route exists in the defined routes
-if (array_key_exists($request, $routes)) {
+if (isset($routes[$request])) {
     // If the route exists, include the corresponding file
     include $routes[$request];
 } else {
     // If the route doesn't exist, show a 404 error or handle it as needed
+    header("HTTP/1.0 404 Not Found");
     echo '404 - Page Not Found';
 }
-
 ?>
